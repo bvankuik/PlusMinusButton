b631373b5b682e1b6c7593d35e0e70d6adabc364
Bart van Kuik <bart@vankuik.nl>
2018-11-22 09:14:35 +0100

    First version

--- PlusMinusButton/ViewController.swift
+++ PlusMinusButton/ViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let plusButton = UIButton()
+    let minusButton = UIButton()
+    let resultLabel = UILabel()
+    var result = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidLayoutSubviews() {
+        let radius = self.resultLabel.frame.height / 2.0
+        self.plusButton.layer.cornerRadius = radius
+        self.minusButton.layer.cornerRadius = radius
+    }
+    
+    @objc func buttonAction(_ sender: UIButton) {
+        print(sender.tag)
+        self.result += sender.tag
+        self.resultLabel.text = String(describing: self.result)
     }
 
+    override func viewDidLoad() {
+        self.plusButton.translatesAutoresizingMaskIntoConstraints = false
+        self.plusButton.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        self.plusButton.tag = 1
+        self.plusButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        self.plusButton.setTitle("+", for: .normal)
+        self.plusButton.titleLabel?.textColor = .white
+        self.plusButton.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        self.view.addSubview(self.plusButton)
+        
+        self.minusButton.translatesAutoresizingMaskIntoConstraints = false
+        self.minusButton.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+        self.minusButton.tag = -1
+        self.minusButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        self.minusButton.setTitle("-", for: .normal)
+        self.minusButton.titleLabel?.textColor = .white
+        self.minusButton.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        self.view.addSubview(self.minusButton)
 
+        self.resultLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.resultLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        self.resultLabel.text = String(describing: self.result)
+        self.resultLabel.textColor = .white
+        self.resultLabel.textAlignment = .center
+        self.view.addSubview(self.resultLabel)
+        
+        [self.plusButton, self.minusButton, self.resultLabel].forEach {
+            $0.backgroundColor = .gray
+        }
+
+        let constraints = [
+            self.minusButton.widthAnchor.constraint(equalTo: self.plusButton.widthAnchor),
+            self.minusButton.rightAnchor.constraint(equalTo: self.resultLabel.leftAnchor),
+            self.minusButton.centerYAnchor.constraint(equalTo: self.resultLabel.centerYAnchor),
+            
+            self.resultLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.resultLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
+            self.resultLabel.heightAnchor.constraint(equalTo: self.minusButton.heightAnchor),
+            self.resultLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            
+            self.plusButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 100),
+            self.plusButton.leftAnchor.constraint(equalTo: self.resultLabel.rightAnchor),
+            self.plusButton.centerYAnchor.constraint(equalTo: self.resultLabel.centerYAnchor),
+        ]
+        self.view.addConstraints(constraints)
+    }
 }
-
